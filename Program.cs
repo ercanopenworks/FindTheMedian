c45c547b28bb96f1127189adb5c572a0926a00db
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-07-04 18:51:01 +0300

    fixed

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindTheMedian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = findMedian(arr);
+
+            Console.WriteLine(result);
+        }
+
+        private static int findMedian(List<int> arr)
+        {
+            
+            var itemsArray = arr.ToArray();
+            Array.Sort(itemsArray);
+
+            return itemsArray[(itemsArray.Length - 1) / 2+1];
+        }
+    }
+}
